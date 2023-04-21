d43f0d5427a39cea4cd51ee5a4e0346065363213
JarvicSena <jsenatin@ucsd.edu>
2023-04-20 17:33:54 -0700

    Fixed ArrayExamples
    ^^^

--- ArrayTests.java
+++ ArrayTests.java
@@ -9,10 +9,30 @@ public class ArrayTests {
     assertArrayEquals(new int[]{ 3 }, input1);
 	}
 
+  @Test
+  public void testReverseInPlace1() {
+    int[] input2 = {2, 4, 6};
+    ArrayExamples.reverseInPlace(input2);
+    assertArrayEquals(new int[]{6, 4, 2}, input2);
+  }
+
+  @Test
+  public void testReverseInPlace2() {
+    int[] input3 = {4, 12};
+    ArrayExamples.reverseInPlace(input3);
+    assertArrayEquals(new int[]{12, 4}, input3);
+  }
+
 
   @Test
   public void testReversed() {
     int[] input1 = { };
     assertArrayEquals(new int[]{ }, ArrayExamples.reversed(input1));
+  }
+
+  @Test
+  public void testReversed1() {
+    int[] input2 = { 1, 2, 3};
+    assertArrayEquals(new int[]{ 3, 2, 1}, ArrayExamples.reversed(input2));
   }
 }
